bacb0521c70e5061819d42829d89259680083f57
skyfury <skyfurynowonline@icloud.com>
2025-03-18 15:07:56 +0800

    完成lab1前7个任务

--- Lab_1/Lab.java
+++ Lab_1/Lab.java
@@ -1,13 +1,22 @@
 package Lab_1;
 
+import org.w3c.dom.ls.LSInput;
+import java.io.IOError;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.Scanner;
 
 public class Lab {
     public static void main(String[] args) {
-        task_1();
-        task_2();
-        task_3();
-        task_4();
+//        task_1();
+//        task_2();
+//        task_3();
+//        task_4();
+//        task_5();
+//        task_6();
+//        task_7();
     }
     static void task_1(){
         for(int i=1;i<=9;i++){
@@ -58,6 +67,113 @@ public class Lab {
         }
     }
     static void task_4(){
+        int n = 10000;
+        boolean [] nums = new boolean[n+1];
+
+        //类似vector的数组
+        ArrayList<Integer>list = new ArrayList<>();
+        //初始假设所有下标对应的数都是素数
+        Arrays.fill(nums,true);
+        for(int i=2;i<=n;i++){
+            if(nums[i]){
+                list.add(i);
+                //如果一个数是素数，则它的倍数一定不是素数
+                for(int j=i*i;j<=n;j+=i){
+                    nums[j]=false;
+                }
+            }
+        }
+        System.out.println("平均值是： "+list.stream().reduce(0,Integer::sum)/n);
+
+        int len = list.size();
+        for(int i=len-1;i>=0;i--){
+            if(list.get(i)<8000){
+                System.out.println("小于 8000 的最大素数是："+ list.get(i));
+                break;
+            }
+        }
+        for(int i=0;i<len;i++){
+            if(list.get(i)>1000){
+                System.out.println("大于 1000 的最小素数是："+ list.get(i));
+                break;
+            }
+        }
+//        /*
+//        平均值是： 573
+//        小于 8000 的最大素数是：7993
+//        大于 1000 的最小素数是：1009
+//        */
+    }
+    static void task_5(){
+        int [][] matrix = new int [5+1][6+1];
+        for(int i=0;i<5;i++){
+            matrix[i][6] = -10000;
+        }
+        for(int j=0;j<6;j++){
+            matrix[5][j] = 10000;
+        }
+        for(int i=0;i<5;i++){
+            for(int j=0;j<6;j++){
+                matrix[i][j]=i*i-(j-i)*(j-i)+10;
+                matrix[i][6] = Math.max(matrix[i][6], matrix[i][j]);
+                matrix[5][j] = Math.min(matrix[5][j], matrix[i][j]);
+            }
+        }
+        System.out.println("数组中每行最大值如下：");
+        for(int i=0;i<5;i++){
+            System.out.print(matrix[i][6]+" ");
+        }
+        System.out.println();
+        System.out.println("数组中每列最小值如下：");
+        for(int j=0;j<6;j++){
+            System.out.print(matrix[5][j]+" ");
+        }
+        /*
+        数组中每行最大值如下：
+        10 11 14 19 26
+        数组中每列最小值如下：
+        10 9 6 1 -6 -15
+        */
+    }
+    static void task_6(){
+        //使用动态规划：C(n,k) = C(n-1,k-1)+C(n-1,k)
+        int n = 30,k = 7;
+//        double [][] C = new double [30+1][7+1];
+        double [][] C = new double [n+1][k+1];
+        for(int i=0;i<=n;i++) {
+            C[i][0] = 1;
+        }
+        for(int j=0;j<=k;j++){
+            C[j][j] = 1;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=k;j++){
+                if(i>=j && i-1>=j){
+                    C[i][j]=C[i-1][j-1]+C[i-1][j];
+                }
+            }
+        }
+        System.out.println("从"+n+"个数中选"+k+"个数，有："+C[n][k]+" 种选法");
+        //从30个数中选7个数，有：2035800.0 种选法
+    }
+    static void task_7(){
+        int n = 100;
+        //存储分母
+        ArrayList<Double> list = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            list.add(2*i-1.0);
+        }
+        double pi = 0.0;
+        for(int i=0;i<n;i++){
+            pi += 1/list.get(i)*Math.pow(-1,i);
+        }
+        System.out.println("估算的pi的值是： "+4*pi);
+        //n=100,估算的pi的值是： 3.1315929035585537
+        //n=1000,估算的pi的值是： 3.140592653839794
+        //n=10000,估算的pi的值是： 3.1414926535900345
+    }
+    static void task_8(){
 
     }
 }
